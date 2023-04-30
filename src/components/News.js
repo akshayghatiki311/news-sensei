24b497e63bccec2c0e6d05f7f1f5897cd17b5e9a
Akshay <akshayghatiki311@gmail.com>
2023-04-30 15:16:25 +0530

    NewsSensei class based component implementation ends here.There are small bugs like first page api is being called twice, need to check that

--- src/components/News.js
+++ src/components/News.js
@@ -2,7 +2,8 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Loader from './Loader';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
     static defaultProps = {
@@ -19,8 +20,9 @@ export default class News extends Component {
     super(props);
     this.state = {
         articles: [],
-        loading: false,
-        page: 1
+        loading: true,
+        page: 1,
+        totalResults: 0
     };
     document.title = `${this.capitalizeWord(this.props.category)}-NewsSensei`;
 }
@@ -29,44 +31,70 @@ capitalizeWord = (string) => {
     return string.charAt(0).toUpperCase()+string.slice(1);
 }
 
+fetchMoreData = async () => {
+    console.log("fetchMoreData");
+    await this.setState({
+        page: this.state.page+1
+    })
+    let apiEndpoint = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`;
+    let data = await fetch(apiEndpoint);
+    let parsedData = await data.json();
+    await this.setState({
+        articles: this.state.articles.concat(parsedData.articles)
+    })
+    // console.log(this.state.articles.length);
+};
+
 updateNews = async () => {
-    let apiEndpoint = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=748eb03f4830456ea48b83b86cbac8f5&pageSize=${this.props.pageSize}&page=${this.state.page}`;
-    this.setState({
+    this.props.setProgress(10);
+    console.log("updateNews");
+    let apiEndpoint = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`;
+    await this.setState({
         loading: true
     })
     let data = await fetch(apiEndpoint);
+    this.props.setProgress(30);
     let parsedData = await data.json();
-    this.setState({
+    this.props.setProgress(50);
+    await this.setState({
         articles: parsedData.articles,
         totalResults: parsedData.totalResults,
         loading: false
-    })
+    });
+    this.props.setProgress(100);
 }
 
 async componentDidMount(){
-    this.updateNews();
+    await this.updateNews();
 }
 
-handleNextClick = async ()=>{
-    this.setState({
-        page: this.state.page+1
-    },()=>{this.updateNews()});
-}
+// handleNextClick = async ()=>{
+//     this.setState({
+//         page: this.state.page+1
+//     },()=>{this.updateNews()});
+// }
 
-handlePrevClick = async ()=>{
-    await this.setState({
-        page: this.state.page-1
-    });
-    this.updateNews();
-}
+// handlePrevClick = async ()=>{
+//     await this.setState({
+//         page: this.state.page-1
+//     });
+//     this.updateNews();
+// }
 
 render() {
     return (
-      <div className="container my-5">
+      <>
       <h1 className="text-center">NewsSensei - Top {this.capitalizeWord(this.props.category)} Headlines</h1>
-      {this.state.loading && <Loader></Loader>}   
+        {this.state.loading && <Loader></Loader>}         
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length<this.state.totalResults}
+          loader={<Loader/>}
+        >
+        <div className="container my-5">
         <div className="row my-5">
-        {!this.state.loading && this.state.articles.map((element)=>{
+        {this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
                 <NewsItem title = {element.title!==null?(element.title.length>45?element.title.slice(0,45)+'...':element.title):"No title"} 
                 description={element.description!==null?(element.description.length>90?element.description.slice(0,90)+'...':element.description):"No Description"} 
@@ -78,11 +106,9 @@ render() {
             </div>
         })}
         </div>
-        <div className="container d-flex justify-content-between">
-            <button type="button" disabled = {this.state.page===1?true:false} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-            <button type="button" disabled = {this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
-      </div>
+        </InfiniteScroll>
+    </>
     )
   }
 }
